ac230df782e318f7c90c701a979bfa7539f0c968
David Ahn <dja1990@gmail.com>
2019-03-08 04:50:59 -0800

    8_kyu_opposite_number complete

---
+++ 8_kyu_opposite_number.js
@@ -0,0 +1,14 @@
+// Very simple, given a number, find its opposite.
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    if (number < 0) {
+        return (-number);
+    } else {
+        return number;
+    }
+  }
